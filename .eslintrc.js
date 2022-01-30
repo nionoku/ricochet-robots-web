823cffe5b5738aba9219abf7afc670b25b9d92a3
nionoku <nionoku@gmail.com>
2022-01-30 21:36:40 +0300

    Add three

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,12 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'no-bitwise': 'off',
+    'no-underscore-dangle': 'off',
+    'no-shadow': 'off',
+    'no-plusplus': 'off',
+    'no-cond-assign': 'off',
   },
 };
